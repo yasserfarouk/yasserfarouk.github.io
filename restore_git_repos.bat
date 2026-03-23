49c0921bac8e0829b664c51394f49dc730410640
Yasser Mohammad <yasserfarouk@gmail.com>
2026-03-23 19:46:48 +0900

    WIP: an intermediary commit for backup

--- restore_git_repos.bat
+++ restore_git_repos.bat
@@ -56,3 +56,32 @@ if exist "sites\yasserfarouk.github.io\.git" (
 )
 echo.
 
+
+REM Repository: sites/yasserfarouk.github.io
+echo Restoring: sites/yasserfarouk.github.io
+if exist "sites\yasserfarouk.github.io\.git" (
+    echo   Directory already exists, skipping...
+) else (
+    REM Create parent directory if needed
+    if not exist "sites" mkdir "sites"
+    
+    REM Clone the repository
+    git clone "git@github.com:yasserfarouk/yasserfarouk.github.io.git" "sites\yasserfarouk.github.io"
+    if errorlevel 1 (
+        echo   Failed to clone
+    ) else (
+        echo   Successfully cloned
+        
+        REM Checkout the original branch if not already on it
+        cd "sites\yasserfarouk.github.io"
+        git checkout "master" 2>nul
+        if errorlevel 1 (
+            echo   Could not checkout branch: master
+        ) else (
+            echo   Checked out branch: master
+        )
+        cd ..\..
+    )
+)
+echo.
+
